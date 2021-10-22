da4307b616e348668a9f7a67ea8745210ab95882
mi5ha6in <mi5ha6in@gmail.com>
2021-10-23 00:26:36 +0300

    adds styled components for previews

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { AppContainer, ColumnContainer, ColumnTitle, CardContainer } from "./styles"
+
+
 export const App = () => {
-  return null;
+  return (
+    <AppContainer>
+      <ColumnContainer>
+        ColumnContainer
+        <ColumnTitle>
+          ColumnTitle
+        </ColumnTitle>
+        <CardContainer>
+          CardContainer
+        </CardContainer>
+      </ColumnContainer>
+      Columns will go here
+    </AppContainer>
+  )
 }
