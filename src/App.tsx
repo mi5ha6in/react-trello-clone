cff04eae841440a9aab858201495c7b890ade47a
mi5ha6in <mi5ha6in@gmail.com>
2021-10-25 01:38:01 +0300

    adds all new components to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { AppContainer} from "./styles"
+import { AppContainer } from "./styles"
 import { Column } from "./Column"
 import { Card } from "./Card"
+import { AddNewItem } from "./AddNewItem"
 
 
 export const App = () => {
   return (
     <AppContainer>
-      <Column text='Title'>
-        <Card text="Card content"></Card>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
       </Column>
-      Columns will go here
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={console.log}
+      />
     </AppContainer>
   )
 }
