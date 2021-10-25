7f5de37650cfe03821ab387dae17cd800930d010
mi5ha6in <mi5ha6in@gmail.com>
2021-10-26 01:40:21 +0300

    adds onKeyPress

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -9,6 +9,13 @@ type NewItemFormProps = {
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("")
   const inputRef = useFocus()
+  const handleAddText = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Enter") {
+      onAdd(text)
+    }
+  }
 
   return (
     <NewItemFormContainer>
@@ -16,6 +23,7 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
         ref={inputRef}
         value={text}
         onChange={e => setText(e.target.value)}
+        onKeyPress={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>
         Create
