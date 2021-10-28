0f3c9ce6da39ffe1c550117110e4eed02f665cb2
mi5ha6in <mi5ha6in@gmail.com>
2021-10-28 18:52:52 +0300

    adds case MOVE_LIST to reducer

--- src/state/AppStateReducer.ts
+++ src/state/AppStateReducer.ts
@@ -37,6 +37,13 @@ export const appStateReducer = (draft: AppState, action: Action): AppState | voi
       })
       break
     }
+    case "MOVE_LIST": {
+      const { draggedId, hoverId } = action.payload
+      const dragIndex = findItemIndexById(draft.lists, draggedId)
+      const hoverIndex = findItemIndexById(draft.lists, hoverId)
+      draft.lists = moveItem(draft.lists, dragIndex, hoverIndex)
+      break
+    }
     default: {
       break
     }
