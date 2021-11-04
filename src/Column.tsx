21f7e9caa8cd314204441cdfafc893f056263855
mi5ha6in <mi5ha6in@gmail.com>
2021-11-04 23:20:20 +0300

    update the layout for using isHidden prop

--- src/Column.tsx
+++ src/Column.tsx
@@ -6,6 +6,7 @@ import { moveList, addTask } from "./state/actions"
 import { useRef } from "react"
 import { useItemDrag } from "./utils/useItemDrag"
 import { useDrop } from "react-dnd"
+import { isHidden } from "./utils/isHidden"
 
 type ColumnProps = {
   text: string
@@ -39,7 +40,7 @@ export const Column = ({ text, id }: ColumnProps) => {
   drag(drop(ref))
 
   return (
-    <ColumnContainer ref={ref}>
+    <ColumnContainer ref={ref} isHidden={isHidden(draggedItem, "COLUMN", id)}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map(task => (
         <Card text={task.text} key={task.id} id={task.id} />
