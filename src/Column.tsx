26cfa9366cf4c76d464ffd756f7503eba846696d
mi5ha6in <mi5ha6in@gmail.com>
2021-11-04 22:53:29 +0300

    adds drop and move for the column

--- src/Column.tsx
+++ src/Column.tsx
@@ -2,9 +2,10 @@ import { ColumnContainer, ColumnTitle } from "./styles"
 import { AddNewItem } from "./AddNewItem"
 import { useAppState } from "./state/AppStateContext"
 import { Card } from "./Card"
-import { addTask } from "./state/actions"
+import { moveList, addTask } from "./state/actions"
 import { useRef } from "react"
 import { useItemDrag } from "./utils/useItemDrag"
+import { useDrop } from "react-dnd"
 
 type ColumnProps = {
   text: string
@@ -12,14 +13,30 @@ type ColumnProps = {
 }
 
 export const Column = ({ text, id }: ColumnProps) => {
-  const { getTasksByListId, dispatch } = useAppState()
+  const { draggedItem, getTasksByListId, dispatch } = useAppState()
 
   const tasks = getTasksByListId(id)
   const ref = useRef<HTMLDivElement>(null)
 
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover() {
+      if (!draggedItem) {
+        return
+      }
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) {
+          return
+        }
+
+        dispatch(moveList(draggedItem.id, id))
+      }
+    }
+  })
+
   const { drag } = useItemDrag({ type: 'COLUMN', id, text })
- 
-  drag(ref)
+
+  drag(drop(ref))
 
   return (
     <ColumnContainer ref={ref}>
