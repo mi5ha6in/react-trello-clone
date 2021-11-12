2bec9d2fb64b8009b83dc70e08da62c87e791b3a
mi5ha6in <mi5ha6in@gmail.com>
2021-11-13 01:07:09 +0300

    fix mistakes

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -22,7 +22,7 @@ export const addTask = (text: string, listId: string,): Action => ({
   type: "ADD_TASK", payload: { text, listId }
 })
 
-export const addList = (text: string): Action => ({ type: "ADD_LIST", pyload: text })
+export const addList = (text: string): Action => ({ type: "ADD_LIST", payload: text })
 
 export const moveList = (draggedId: string, hoverId: string): Action => (
   {
