455732d34dee550680d7af69065f1217f36e5b1f
mi5ha6in <mi5ha6in@gmail.com>
2021-11-13 01:05:15 +0300

    add prop isPreview

--- src/styles.ts
+++ src/styles.ts
@@ -2,9 +2,11 @@ import styled from "styled-components"
 
 interface DragPreviewContainerProps {
   isHidden?: boolean
+  isPreview?: boolean
 }
 
 export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
+  transform: ${props => (props.isPreview ? "rotate(5deg)" : undefined)};
   opacity: ${props => props.isHidden ? 0.3 : 1};
 `
 
