573a78960949bd06a9c39909ad65dc06acf44187
mi5ha6in <mi5ha6in@gmail.com>
2021-11-17 00:18:54 +0300

    add types to CardProps

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,8 +3,10 @@ import { CardContainer } from "./styles"
 type CardProps = {
   text: string
   id: string
+  columnId: string
+  isPreview?: boolean
 }
 
-export const Card = ({ text }: CardProps) => {
+export const Card = ({ text, id, columnId, isPreview }: CardProps) => {
   return <CardContainer>{text}</CardContainer>
 }
