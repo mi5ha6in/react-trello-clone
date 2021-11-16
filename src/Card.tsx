96b0dde0fb2ce5ceda8ac1b7381b7f32e0508e7d
mi5ha6in <mi5ha6in@gmail.com>
2021-11-17 00:32:16 +0300

    update Card for dragg function

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,10 @@
+import { useRef } from "react"
 import { CardContainer } from "./styles"
+import { useItemDrag } from "./utils/useItemDrag"
+import { useDrop } from "react-dnd"
+import { useAppState } from "./state/AppStateContext"
+import { isHidden } from "./utils/isHidden"
+import { moveTask, setDraggedItem } from "./state/actions"
 
 type CardProps = {
   text: string
@@ -8,5 +14,39 @@ type CardProps = {
 }
 
 export const Card = ({ text, id, columnId, isPreview }: CardProps) => {
-  return <CardContainer>{text}</CardContainer>
+  const { draggedItem, dispatch } = useAppState()
+  const ref = useRef<HTMLDivElement>(null)
+  const { drag } = useItemDrag({
+    type: "CARD",
+    id,
+    text,
+    columnId
+  })
+  const [, drop] = useDrop({
+    accept: "CARD",
+    hover() {
+      if (!draggedItem) {
+        return
+      }
+      if (draggedItem.type !== "CARD") {
+        return
+      }
+      if (draggedItem.id === id) {
+        return
+      }
+
+      dispatch(
+        moveTask(draggedItem.id, id, draggedItem.columnId, columnId)
+      )
+    }
+  })
+
+  drag(drop(ref))
+
+  return <CardContainer>
+    isHidden={isHidden(draggedItem, "CARD", id, isPreview)}
+    isPreview={isPreview}
+    ref={ref}
+    {text}
+  </CardContainer>
 }
